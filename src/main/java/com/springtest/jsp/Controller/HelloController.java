fd16309999bdd0e39291c631197b4b947dbeace7
1284783601 <1284783601@qq.com>
2019-04-26 10:34:45 +0800

    first commit

---
+++ src/main/java/com/springtest/jsp/Controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.springtest.jsp.Controller;
+import java.text.DateFormat;
+import java.util.Date;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(Model m){
+        m.addAttribute("now",DateFormat.getDateTimeInstance().format(new Date()));
+        return "hello";  //视图重定向hello.jsp
+    }
+}
